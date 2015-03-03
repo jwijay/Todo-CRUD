ac793e87771f2fe4c97dc5f4d1ee329e4bab5088
Jacqueline Wijaya <jackie.wijaya@gmail.com>
2015-03-02 16:27:27 -1000

    stuff saves to todo database now. hooray

--- app.js
+++ app.js
@@ -21,14 +21,40 @@ var Todo = mongoose.model('Todo', todoSchema);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 app.set('view engine', 'jade');
 
 app.get('/', function(req, res) {
   res.render('list');
 });
 
+app.get('/new_todo', function(req, res) {
+  res.render('new_todo');
+});
+
+app.post('/todos', function(req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+
+  var todo = new Todo(
+  {
+    title : title,
+    description : description,
+    is_done : false,
+    created_at : new Date()
+  });
+
+  todo.save(function(err) {
+    if(err) throw err;
+    res.redirect('/');
+  });
+});
 
 //methodOverride(getter, options)
+//HANDLE DELETE ROUTE HERE
+app.delete('/todos/:id', function() {
+  //JK do it here
+});
 
 var server = app.listen(3000, function() {
   var host = server.address().address;
